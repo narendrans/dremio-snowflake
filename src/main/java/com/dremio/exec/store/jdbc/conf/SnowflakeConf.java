00f1bbd5c16c99224bf8feeb5eb4d330896f2d3d
Naren <naren@bigsur.local>
2020-12-01 18:46:58 -0500

    WIP #34

--- src/main/java/com/dremio/exec/store/jdbc/conf/SnowflakeConf.java
+++ src/main/java/com/dremio/exec/store/jdbc/conf/SnowflakeConf.java
@@ -18,8 +18,6 @@ package com.dremio.exec.store.jdbc.conf;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import javax.sql.DataSource;
-
 import com.dremio.exec.store.jdbc.*;
 import com.dremio.options.OptionManager;
 import com.dremio.security.CredentialsService;
@@ -40,121 +38,104 @@ import io.protostuff.Tag;
 @SourceType(value = "SNOWFLAKE", label = "Snowflake")
 public class SnowflakeConf extends AbstractArpConf<SnowflakeConf> {
 
-  private static final String ARP_FILENAME = "arp/implementation/snowflake-arp.yaml";
-  private static final ArpDialect ARP_DIALECT =
-      AbstractArpConf.loadArpFile(ARP_FILENAME, (ArpDialect::new));
-  private static final String DRIVER = "net.snowflake.client.jdbc.SnowflakeDriver";
-  private static Logger logger = Logger.getLogger(SnowflakeConf.class);
+    private static final String ARP_FILENAME = "arp/implementation/snowflake-arp.yaml";
+    private static final ArpDialect ARP_DIALECT =
+            AbstractArpConf.loadArpFile(ARP_FILENAME, (ArpDialect::new));
+    private static final String DRIVER = "net.snowflake.client.jdbc.SnowflakeDriver";
+    private static Logger logger = Logger.getLogger(SnowflakeConf.class);
 
-  /*
-    The following block is required as Snowflake reports integers as NUMBER(38,0).
-   */
-  static class SnowflakeSchemaFetcher implements JdbcSchemaFetcher {
+    /*
+        TODO: Investigate and fix this. Due to changes in the API and how Snowflake uses NUMBER(38,0)
+        to represent integer the following error is thrown if a table containing decimals is queried
+        IllegalArgumentException: Gandiva only supports decimals of upto 38 precision. Input precision : 0.
+     */
+    static class SnowflakeSchemaFetcher extends JdbcSchemaFetcherImpl {
 
-    public SnowflakeSchemaFetcher(String name, DataSource dataSource, int timeout, Config config) {
-      super();
+        public SnowflakeSchemaFetcher(String name, int timeout, Config config) {
+            super(name, timeout, config);
+        }
+
+        protected boolean usePrepareForColumnMetadata() {
+            return true;
+        }
     }
 
-    protected boolean usePrepareForColumnMetadata() {
-      return true;
+    static class SnowflakeDialect extends ArpDialect {
+
+        public SnowflakeDialect(ArpYaml yaml) {
+            super(yaml);
+        }
+
+        @Override
+        public JdbcSchemaFetcherImpl getSchemaFetcher(String name, int timeout, Config config) {
+            return new SnowflakeSchemaFetcher(name, timeout, config);
+        }
+
+        public boolean supportsNestedAggregations() {
+            return false;
+        }
+    }
+
+
+    /*
+       Check Snowflake JDBC connection docs for more details: https://docs.snowflake.net/manuals/user-guide/jdbc-configure.html
+     */
+    @Tag(1)
+    @DisplayMetadata(label = "JDBC URL (Ex: jdbc:snowflake://<account_name>.snowflakecomputing.com/?param1=value&param2=value)")
+    public String jdbcURL;
+
+    @Tag(2)
+    @DisplayMetadata(label = "Username")
+    public String username;
+
+    @Tag(3)
+    @Secret
+    @DisplayMetadata(label = "Password")
+    public String password;
+
+    @Tag(4)
+    @DisplayMetadata(label = "Record fetch size")
+    @NotMetadataImpacting
+    public int fetchSize = 2000;
+
+    @Tag(5)
+    @NotMetadataImpacting
+    @DisplayMetadata(label = "Grant External Query access (External Query allows creation of VDS from a Snowflake query. Learn more here: https://docs.dremio.com/data-sources/external-queries.html#enabling-external-queries)")
+    public boolean enableExternalQuery = false;
+
+    @VisibleForTesting
+    public String toJdbcConnectionString() {
+        checkNotNull(this.jdbcURL, "JDBC URL is required");
+        return jdbcURL;
     }
 
     @Override
-    public JdbcFetcherProto.GetStateResponse getState(JdbcFetcherProto.GetStateRequest getStateRequest) {
-      return null;
+    @VisibleForTesting
+    public Config toPluginConfig(CredentialsService credentialsService, OptionManager optionManager) {
+        logger.info("Connecting to Snowflake");
+        return JdbcStoragePlugin.Config.newBuilder()
+                .withDialect(getDialect())
+                .withFetchSize(fetchSize)
+                .withDatasourceFactory(this::newDataSource)
+                .clearHiddenSchemas()
+                .addHiddenSchema("SYSTEM")
+                .withAllowExternalQuery(enableExternalQuery)
+                .build();
+    }
+
+    private CloseableDataSource newDataSource() {
+        return DataSources.newGenericConnectionPoolDataSource(DRIVER,
+                toJdbcConnectionString(), username, password, null,
+                DataSources.CommitMode.DRIVER_SPECIFIED_COMMIT_MODE);
     }
 
     @Override
-    public JdbcFetcherProto.CatalogOrSchemaExistsResponse catalogOrSchemaExists(JdbcFetcherProto.CatalogOrSchemaExistsRequest catalogOrSchemaExistsRequest) {
-      return null;
+    public ArpDialect getDialect() {
+        return ARP_DIALECT;
     }
 
-    @Override
-    public void start() throws Exception {
-
+    @VisibleForTesting
+    public static ArpDialect getDialectSingleton() {
+        return ARP_DIALECT;
     }
-
-    @Override
-    public void close() throws Exception {
-
-    }
-  }
-
-  static class SnowflakeDialect extends ArpDialect {
-
-    public SnowflakeDialect(ArpYaml yaml) {
-      super(yaml);
-    }
-
-    public JdbcSchemaFetcher getSchemaFetcher(String name, DataSource dataSource, int timeout,
-        JdbcStoragePlugin.Config config) {
-      return new SnowflakeSchemaFetcher(name, dataSource, timeout, config);
-    }
-
-    public boolean supportsNestedAggregations() {
-      return false;
-    }
-  }
-
-  /*
-     Check Snowflake JDBC connection docs for more details: https://docs.snowflake.net/manuals/user-guide/jdbc-configure.html
-   */
-  @Tag(1)
-  @DisplayMetadata(label = "JDBC URL (Ex: jdbc:snowflake://<account_name>.snowflakecomputing.com/?param1=value&param2=value)")
-  public String jdbcURL;
-
-  @Tag(2)
-  @DisplayMetadata(label = "Username")
-  public String username;
-
-  @Tag(3)
-  @Secret
-  @DisplayMetadata(label = "Password")
-  public String password;
-
-  @Tag(4)
-  @DisplayMetadata(label = "Record fetch size")
-  @NotMetadataImpacting
-  public int fetchSize = 2000;
-
-  @Tag(5)
-  @NotMetadataImpacting
-  @DisplayMetadata(label = "Grant External Query access (External Query allows creation of VDS from a Snowflake query. Learn more here: https://docs.dremio.com/data-sources/external-queries.html#enabling-external-queries)")
-  public boolean enableExternalQuery = false;
-
-  @VisibleForTesting
-  public String toJdbcConnectionString() {
-    checkNotNull(this.jdbcURL, "JDBC URL is required");
-    return jdbcURL;
-  }
-
-  @Override
-  @VisibleForTesting
-  public Config toPluginConfig(CredentialsService credentialsService, OptionManager optionManager) {
-    logger.info("Connecting to Snowflake");
-    return JdbcStoragePlugin.Config.newBuilder()
-        .withDialect(getDialect())
-        .withFetchSize(fetchSize)
-        .withDatasourceFactory(this::newDataSource)
-        .clearHiddenSchemas()
-        .addHiddenSchema("SYSTEM")
-        .withAllowExternalQuery(enableExternalQuery)
-        .build();
-  }
-
-  private CloseableDataSource newDataSource() {
-    return DataSources.newGenericConnectionPoolDataSource(DRIVER,
-        toJdbcConnectionString(), username, password, null,
-        DataSources.CommitMode.DRIVER_SPECIFIED_COMMIT_MODE);
-  }
-
-  @Override
-  public ArpDialect getDialect() {
-    return ARP_DIALECT;
-  }
-
-  @VisibleForTesting
-  public static ArpDialect getDialectSingleton() {
-    return ARP_DIALECT;
-  }
 }
